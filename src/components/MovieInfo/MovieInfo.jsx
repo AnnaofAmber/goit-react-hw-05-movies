62e62fbf58ae105380678631eb413a2aa2bf3856
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-12 20:38:53 +0300

    adding styles for Movie info

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,13 +1,17 @@
+import css from "./MovieInfo.module.css"
+
 export const MovieInfo = ({movie, genre})=>{
     const {title, vote_average, poster_path, overview} = movie
   
     return(
-        <div>
-            <img src={poster_path?`https://image.tmdb.org/t/p/w300/${poster_path}`:'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700'} alt={title} />
-            <h2>{title}</h2>
-            <p>{genre}</p>
-            <p>{overview}</p>
-            <p>{(vote_average * 10).toFixed(1)}%</p>
+        <div className={css.movie_container}>
+            <img className={css.img} src={poster_path?`https://image.tmdb.org/t/p/w300/${poster_path}`:'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700'} alt={title} />
+            <div className={css.info}>
+            <h2 className={css.title}>{title}</h2>
+            <p className={css.genre}>{genre}</p>
+            <p className={css.overview}>{overview}</p>
+            <p className={css.vote}>{(vote_average * 10).toFixed(1)}%</p>
+            </div>
         </div>
     )
 }

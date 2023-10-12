a61db857645ac003e5a93e3f6c9086eff7123d5b
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-12 19:41:43 +0300

    adding styles for cast

--- src/components/MovieCast/MovieCastItem.jsx
+++ src/components/MovieCast/MovieCastItem.jsx
@@ -1,9 +1,13 @@
+import css from "./MovieCastItem.module.css"
+
 export const MovieCastItem = ({image, name, role})=>{
     return(
-        <li>
-            <img src={image} alt={name} />
-            <p>Name: {name}</p>
-            <p>Character: {role}</p>
+        <li className={css.item}>
+            <img className={css.img} src={image} alt={name} />
+            <div className={css.info}>
+            <p className={css.title}>Name: {name}</p>
+            <p className={css.role}>Character: {role}</p>
+            </div>
         </li>
     )
 }

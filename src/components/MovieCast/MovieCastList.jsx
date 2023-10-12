d67711bea9f8224c39e37fe8d52ea3e2e33cad17
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-12 20:19:24 +0300

    adding styles for cast list

--- src/components/MovieCast/MovieCastList.jsx
+++ src/components/MovieCast/MovieCastList.jsx
@@ -1,8 +1,10 @@
+import css from "./MovieCastList.module.css"
+
 import { MovieCastItem } from "./MovieCastItem";
 
 export const MovieCastList = ({data}) =>{
 return(
-    <ul>
+    <ul className={css.list}>
         {data.map(e=> (
         <MovieCastItem key={e.id} image={e.profile_path? `https://image.tmdb.org/t/p/w300/${e.profile_path}`:'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700'} name={e.name} role={e.character}/>)
         )}
